a951172f808f6a218d9e4c09b9eb06c22efff5ba
Romario Mcgregor <40338784+codingthefuture@users.noreply.github.com>
2023-01-04 20:39:04 -0500

    cors correction for front end

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://fakeflix-api.onrender.com'
+    'https://fakeflix-api.onrender.com',
+    'https://master--fakeflix-ui.netlify.app'
 ]
 
 module.exports = allowedOrigins
